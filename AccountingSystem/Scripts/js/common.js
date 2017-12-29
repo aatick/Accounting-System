acaeae5e227bb9ba732c619226920eb44aa9fdbb
aatick <aatick.ru3@gmail.com>
2017-12-29 23:31:54 +0600

    Add files via upload

---
+++ AccountingSystem/Scripts/js/common.js
@@ -0,0 +1,6 @@
+﻿$(document).mouseup(function (e) {
+    var container = $(".list-container");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        $("ul.export-container").removeClass("active");
+    }
+});
